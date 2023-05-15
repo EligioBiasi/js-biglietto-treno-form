120b55742e22af4f1ed8ffbbfba737d2280b88ca
EligioBiasi <biasi.eligio@edufaimm.it>
2023-05-15 16:07:14 +0200

    attempt fix

--- script.js
+++ script.js
@@ -8,23 +8,28 @@ const button = document.querySelector('button.send');
 
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
 // il prezzo del biglietto è definito in base ai km (0.233 € al km)
-let costPerKm = inputKilometers * 0.233;
-let discount;
+
 // va applicato uno sconto del 19.4% per i minorenni
 // va applicato uno sconto del 37.7% per gli over 65.
-if (inputAge<18){
-    discount = ((costPerKm * 19.4) / 100);
-} else if (inputAge > 65){
-    discount = ((costPerKm * 37.7) / 100);
-} else{
-    discount = 0;
-}
-const finalcost = costPerKm - discount;
 
 button.addEventListener('click', function(){
-    console.log(document.getElementById('final-price').innerHTML += finalcost)
-})
+    let costPerKm = inputKilometers * 0.233;
+    let discount;
+    if (inputAge<18){
+        discount = ((costPerKm * 19.4) / 100);
+    } else if (inputAge > 65){
+        discount = ((costPerKm * 37.7) / 100);
+    } else{
+        discount = 0;
+    }
+    const finalcost = costPerKm - discount;
+    const decimal = finalcost.toFixed(2);
+    console.log(document.getElementById('final-price').innerHTML += finalcost);
+});
 
+
+
+    
 
 
 
